da7f5af045da303498ceedcf4282271712e861eb
yyp <1027980867@qq.com>
2023-09-19 17:37:30 +0800

    update date

---
+++ src/main/java/com/banfftech/handler/OverrideMemberFindList.java
@@ -0,0 +1,85 @@
+package com.banfftech.handler;
+
+import com.dpbird.odata.OfbizODataException;
+import com.dpbird.odata.edm.OdataOfbizEntity;
+import com.dpbird.odata.handler.DefaultEntityHandler;
+import com.dpbird.odata.handler.HandlerResults;
+import org.apache.ofbiz.base.util.UtilMisc;
+import org.apache.ofbiz.base.util.UtilValidate;
+import org.apache.ofbiz.entity.Delegator;
+import org.apache.ofbiz.entity.GenericEntityException;
+import org.apache.ofbiz.entity.GenericValue;
+import org.apache.ofbiz.entity.condition.EntityCondition;
+import org.apache.ofbiz.entity.condition.EntityOperator;
+import org.apache.ofbiz.entity.util.EntityQuery;
+import org.apache.olingo.commons.api.edm.EdmBindingTarget;
+import org.apache.olingo.commons.api.edm.EdmNavigationProperty;
+import org.apache.olingo.server.api.uri.queryoption.QueryOption;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @ClassName: OverrideEnumerationFindList
+ * @Description: TODO
+ * @Author: banff
+ * @Date: 2023/8/10 12:16
+ */
+public class OverrideMemberFindList extends DefaultEntityHandler {
+
+    @Override
+    public HandlerResults findList(Map<String, Object> odataContext, EdmBindingTarget edmBindingTarget, Map<String, Object> primaryKey,
+                                   Map<String, QueryOption> queryOptions, Map<String, Object> navigationParam) throws OfbizODataException {
+        Delegator delegator = (Delegator) odataContext.get("delegator");
+
+        //普通的查询
+        if (UtilValidate.isNotEmpty(navigationParam)) {
+            EdmNavigationProperty edmNavigationProperty = (EdmNavigationProperty) navigationParam.get("edmNavigationProperty");
+            String navigationPropertyName = edmNavigationProperty.getName();
+            if ("AllMember".equals(navigationPropertyName)) {
+
+                OdataOfbizEntity entity = (OdataOfbizEntity) navigationParam.get("entity");
+                GenericValue genericValue = entity.getGenericValue();
+
+                //如果是多段式的且查所有的成员
+                String departmentId = genericValue.getString("partyId");
+                List<GenericValue> allMembers = new ArrayList<>();
+                getDepartmentALlMembers(delegator, departmentId, allMembers);
+
+                return new HandlerResults(allMembers.size(), allMembers);
+            }
+        }
+        return super.findList(odataContext, edmBindingTarget, primaryKey, queryOptions, navigationParam);
+
+
+    }
+
+    private void getDepartmentALlMembers(Delegator delegator, String departmentId, List<GenericValue> allMembers)
+            throws OfbizODataException {
+        //获取该部门的所有子部门
+        try {
+            List<GenericValue> subDepartments = EntityQuery.use(delegator).from("PartyRelationship")
+                    .where(UtilMisc.toMap("partyIdFrom", departmentId, "roleTypeIdTo", "DEPARTMENT"))
+                    .queryList();
+
+            //获取当前部门的所有成员列表
+            List<GenericValue> membersId = EntityQuery.use(delegator).from("PartyRelationship")
+                    .where(UtilMisc.toMap("partyIdFrom", departmentId, "roleTypeIdTo", "ORD_EMPLOYEE"))
+                    .getFieldList("partyIdTo");
+
+            EntityCondition membersIdCondition = EntityCondition.makeCondition("partyId", EntityOperator.IN, membersId);
+            List<GenericValue> members = EntityQuery.use(delegator).from("PartyAndContact").where(membersIdCondition).queryList();
+            allMembers.addAll(members);
+
+            if (UtilValidate.isNotEmpty(subDepartments)) {
+                for (GenericValue subDepartment : subDepartments) {
+                    getDepartmentALlMembers(delegator, subDepartment.getString("partyIdTo"), allMembers);
+                }
+            }
+            return;
+        } catch (GenericEntityException e) {
+            throw new OfbizODataException(e.getMessage());
+        }
+    }
+}
--- src/main/java/com/banfftech/services/GConfigService.java
+++ src/main/java/com/banfftech/services/GConfigService.java
@@ -1,17 +1,24 @@
 package com.banfftech.services;
 
 import com.banfftech.common.util.CommonUtils;
+import com.banfftech.util.ServiceUtils;
+import com.dpbird.odata.OfbizODataException;
 import com.dpbird.odata.services.OfbizServiceException;
 import org.apache.ofbiz.base.util.UtilMisc;
 import org.apache.ofbiz.base.util.UtilValidate;
 import org.apache.ofbiz.entity.Delegator;
 import org.apache.ofbiz.entity.GenericEntityException;
 import org.apache.ofbiz.entity.GenericValue;
+import org.apache.ofbiz.entity.condition.EntityCondition;
+import org.apache.ofbiz.entity.condition.EntityOperator;
 import org.apache.ofbiz.entity.util.EntityQuery;
 import org.apache.ofbiz.service.*;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Map;
+
+import static com.banfftech.util.ServiceUtils.traverseUpdateParentDepartments;
 
 /**
  * @ClassName: GConfigService
@@ -82,6 +89,14 @@ public class GConfigService {
         return ServiceUtil.returnSuccess();
     }
 
+    /**
+     * @param [dctx, context]
+     * @Author yyp
+     * @Description 主要功能:在某个部门添加成员之后,更新该部门及其所有父级部门的数量
+     * @Date 11:24 2023/9/19
+     * @EntityTypeName PartyGroup
+     * @ServiceName banfftech.updateMemberNumber
+     **/
     public static Map<String, Object> updateMemberNumber(DispatchContext dctx, Map<String, Object> context)
             throws GenericServiceException, OfbizServiceException {
         try {
@@ -89,20 +104,23 @@ public class GConfigService {
             LocalDispatcher dispatcher = dctx.getDispatcher();
             GenericValue userLogin = (GenericValue) context.get("userLogin");
             String partyId = (String) context.get("partyId");
+            //使用成员ID查询所在部门(一个成员只能有个一个部门)
             GenericValue department = EntityQuery.use(delegator).from("PartyRelationship")
                     .where(UtilMisc.toMap("partyIdTo", partyId, "roleTypeIdTo", "ORD_EMPLOYEE"))
                     .queryFirst();
-            if(UtilValidate.isEmpty(department)){
+            if (UtilValidate.isEmpty(department)) {
                 throw new OfbizServiceException("没有找到当前员工所在部门");
             }
-            List<GenericValue> members = EntityQuery.use(delegator).from("PartyRelationship")
-                    .where(UtilMisc.toMap("partyIdFrom", department.getString("partyIdFrom"), "roleTypeIdTo", "ORD_EMPLOYEE"))
-                    .queryList();
-            dispatcher.runSync("banfftech.updatePartyGroup",UtilMisc.toMap("partyId",department.getString("partyIdFrom"),"numEmployees",members.size(),"userLogin",userLogin));
-        } catch (GenericEntityException e) {
+            //当前成员所在部门Id
+            String currentDepartmentId = department.getString("partyIdFrom");
+            //从当前部们开始遍历知道根部门为止(暂时根部们先定死为Company,今后考虑怎么改为动态获取根部们)
+            ServiceUtils.traverseUpdateParentDepartments(delegator, dispatcher, currentDepartmentId, userLogin);
+        } catch (GenericEntityException | OfbizODataException e) {
             throw new GenericServiceException(e.getMessage());
         }
 
         return ServiceUtil.returnSuccess();
     }
+
+
 }
