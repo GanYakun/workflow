d113d4ff79aa8239a3f57bb1dc28dc64ca9e38c5
SunCanyang <try028@163.com>
2023-09-21 11:16:25 +0800

    更新审批

--- src/main/java/com/banfftech/event/ApprovalEvent.java
+++ src/main/java/com/banfftech/event/ApprovalEvent.java
@@ -1,7 +1,6 @@
 package com.banfftech.event;
 
 import com.dpbird.odata.OfbizODataException;
-import com.dpbird.odata.Util;
 import com.dpbird.odata.edm.OdataOfbizEntity;
 import net.sf.json.JSONObject;
 import org.apache.ofbiz.base.util.UtilDateTime;
@@ -13,7 +12,6 @@ import org.apache.ofbiz.entity.GenericValue;
 import org.apache.ofbiz.entity.condition.EntityCondition;
 import org.apache.ofbiz.entity.model.ModelEntity;
 import org.apache.ofbiz.entity.util.EntityQuery;
-import org.apache.ofbiz.service.GenericServiceException;
 import org.apache.ofbiz.service.LocalDispatcher;
 import org.apache.olingo.commons.api.edm.EdmBindingTarget;
 
@@ -31,9 +29,8 @@ public class ApprovalEvent {
      * 流程设计发布,客户端会传递完整的流程json,保存它
      */
     public static void saveProcess(Map<String, Object> oDataContext, Map<String, Object> actionParameters,
-                                   EdmBindingTarget edmBindingTarget) throws GenericEntityException, GenericServiceException {
+                                   EdmBindingTarget edmBindingTarget) throws GenericEntityException {
         Delegator delegator = (Delegator) oDataContext.get("delegator");
-        LocalDispatcher dispatcher = (LocalDispatcher) oDataContext.get("dispatcher");
         OdataOfbizEntity ofbizEntity = (OdataOfbizEntity) actionParameters.get("mainProcess");
         //所有的节点数据
         String nodeData = (String) actionParameters.get("nodeData");
@@ -42,8 +39,8 @@ public class ApprovalEvent {
         if (UtilValidate.isEmpty(workFlowId)) {
             //创建流程
             String noteId = delegator.getNextSeqId("NoteData");
-            delegator.create("NoteData", UtilMisc.toMap("noteId", noteId, "noteName", "FLOW_JSON", "noteInfo", nodeData, "noteDateTime", UtilDateTime.nowTimestamp()));
-
+            delegator.create("NoteData", UtilMisc.toMap("noteId", noteId, "noteName", "FLOW_JSON",
+                    "statusId", "PROCESS_ENABLED", "noteInfo", nodeData, "noteDateTime", UtilDateTime.nowTimestamp()));
             String workEffortId = delegator.getNextSeqId("WorkEffort");
             delegator.create("WorkEffort", UtilMisc.toMap("workEffortId", workEffortId, "workEffortTypeId", "WORK_FLOW", "noteId", noteId));
             process.set("workFlowId", workEffortId);
@@ -53,6 +50,7 @@ public class ApprovalEvent {
             GenericValue workEffort = process.getRelatedOne("WorkEffort", false);
             GenericValue noteData = workEffort.getRelatedOne("NoteData", false);
             noteData.set("noteInfo", nodeData);
+            noteData.set("statusId", "PROCESS_ENABLED");
             noteData.set("noteDateTime", UtilDateTime.nowTimestamp());
             noteData.store();
         }
@@ -62,7 +60,7 @@ public class ApprovalEvent {
      * 发布
      */
     public static void publishFlow(Map<String, Object> oDataContext, Map<String, Object> actionParameters,
-                                   EdmBindingTarget edmBindingTarget) throws GenericEntityException, GenericServiceException {
+                                   EdmBindingTarget edmBindingTarget) throws GenericEntityException {
         Delegator delegator = (Delegator) oDataContext.get("delegator");
         LocalDispatcher dispatcher = (LocalDispatcher) oDataContext.get("dispatcher");
         OdataOfbizEntity ofbizEntity = (OdataOfbizEntity) actionParameters.get("mainProcess");
@@ -74,7 +72,7 @@ public class ApprovalEvent {
      * 停用
      */
     public static void stopFlow(Map<String, Object> oDataContext, Map<String, Object> actionParameters,
-                                   EdmBindingTarget edmBindingTarget) throws GenericEntityException, GenericServiceException {
+                                   EdmBindingTarget edmBindingTarget) throws GenericEntityException {
         Delegator delegator = (Delegator) oDataContext.get("delegator");
         LocalDispatcher dispatcher = (LocalDispatcher) oDataContext.get("dispatcher");
         OdataOfbizEntity ofbizEntity = (OdataOfbizEntity) actionParameters.get("mainProcess");
@@ -93,29 +91,28 @@ public class ApprovalEvent {
         String typeId = (String) actionParameters.get("type");
         GenericValue genericValue = ofbizEntity.getGenericValue();
         String entityName = genericValue.getEntityName();
-//        String typeId = genericValue.getString(Util.firstLowerCase(entityName) + "TypeId");
         GenericValue processEntity = EntityQuery.use(delegator).from("ProcessEntity")
                 .where("processEntityName", entityName, "processEntityTypeId", typeId).queryFirst();
         if (UtilValidate.isEmpty(processEntity)) {
             throw new OfbizODataException("业务对象不存在: " + entityName);
         }
         //获取主流程对象
-        GenericValue mainProcess = EntityQuery.use(delegator).from("MainProcess").where(processEntity.getPrimaryKey()).queryFirst();
+        GenericValue mainProcess = EntityQuery.use(delegator).from("MainProcess")
+                .where(UtilMisc.toMap("statusId", "PROCESS_ENABLED", "processEntityId", processEntity.getString("processEntityId"))).queryFirst();
         if (UtilValidate.isEmpty(mainProcess) || UtilValidate.isEmpty(mainProcess.getString("workFlowId"))) {
-            throw new OfbizODataException("未配置审批流程");
+            throw new OfbizODataException("未找到有效的审批流程");
         }
         GenericValue templateWorkEffort = mainProcess.getRelatedOne("WorkEffort", false);
         GenericValue noteData = templateWorkEffort.getRelatedOne("NoteData", false);
         String flowJson = noteData.getString("noteInfo");
         JSONObject jsonObject = JSONObject.fromObject(flowJson);
 
-        String workEffortId = delegator.getNextSeqId("WorkEffort");
-        Long revisionNumber = delegator.getNextSeqIdLong("RevisionNumber");
         //创建根节点
+        String workEffortId = delegator.getNextSeqId("WorkEffort");
         GenericValue rootWorkEffort = delegator.create("WorkEffort", UtilMisc.toMap("workEffortId", workEffortId,
                 "workEffortName", jsonObject.getString("nodeName"), "workEffortTypeId", "ROOT_NODE",
                 "priority", jsonObject.getLong("nodeId"), "workEffortParentId", templateWorkEffort.getString("workEffortId"),
-                "revisionNumber", revisionNumber, "createdByUserLogin",  userLogin.getString("userLoginId")));
+                "revisionNumber", delegator.getNextSeqIdLong("RevisionNumber"), "createdByUserLogin",  userLogin.getString("userLoginId")));
         //把审批对象关联到根节点
         ModelEntity modelEntity = genericValue.getModelEntity();
         //重复提交
