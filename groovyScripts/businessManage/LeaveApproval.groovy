6f030de19c297b165c8c65f0d58e4884d579dcc2
yyp <1027980867@qq.com>
2023-09-27 14:22:54 +0800

    update

--- groovyScripts/businessManage/LeaveApproval.groovy
+++ groovyScripts/businessManage/LeaveApproval.groovy
@@ -9,14 +9,18 @@ def generateFields(Map<String, Object> context) {
     List<OdataOfbizEntity> entityList = context.parameters.entityList;
     entityList.each { entity ->
         Boolean isHiddenSubmitApproval = true;
-
+        Boolean isHiddenCancelApproval = true;
         GenericValue leaveApproval = (GenericValue) entity.getGenericValue();
         String leaveStatus = leaveApproval.getString("statusId");
         if("APPROVAL_CREATED".equals(leaveStatus) ||"APPROVAL_REJECTED".equals(leaveStatus)) {
             isHiddenSubmitApproval = false;
         }
+        if("APPROVAL_SUBMITTED".equals(leaveStatus)) {
+            isHiddenSubmitApproval = false;
+        }
 
         entity.addProperty("isHiddenSubmitApproval", isHiddenSubmitApproval);
+        entity.addProperty("isHiddenCancelApproval", isHiddenCancelApproval);
     }
 
     return entityList;
