419a506a0ad152860a8056b004258fd6a2ee414a
yyp <1027980867@qq.com>
2023-10-12 17:16:38 +0800

    实现部添加人员和变更人员部门时的人数更新

--- src/main/java/com/banfftech/services/GConfigService.java
+++ src/main/java/com/banfftech/services/GConfigService.java
@@ -4,25 +4,18 @@ import com.banfftech.common.util.CommonUtils;
 import com.banfftech.util.ServiceUtils;
 import com.dpbird.odata.OfbizODataException;
 import com.dpbird.odata.services.OfbizServiceException;
-import org.apache.ofbiz.base.crypto.HashCrypt;
 import org.apache.ofbiz.base.util.UtilDateTime;
 import org.apache.ofbiz.base.util.UtilMisc;
 import org.apache.ofbiz.base.util.UtilValidate;
 import org.apache.ofbiz.entity.Delegator;
 import org.apache.ofbiz.entity.GenericEntityException;
 import org.apache.ofbiz.entity.GenericValue;
-import org.apache.ofbiz.entity.condition.EntityCondition;
-import org.apache.ofbiz.entity.condition.EntityOperator;
 import org.apache.ofbiz.entity.util.EntityQuery;
-import org.apache.ofbiz.entity.util.EntityUtilProperties;
 import org.apache.ofbiz.service.*;
 
-import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-
-import static com.banfftech.util.ServiceUtils.traverseUpdateParentDepartments;
-import static org.apache.ofbiz.common.login.LoginServices.getHashType;
 
 /**
  * @ClassName: GConfigService
@@ -125,24 +118,51 @@ public class GConfigService {
      * @EntityTypeName PartyGroup
      * @ServiceName banfftech.updateMemberNumber
      **/
+//    public static Map<String, Object> updateMemberNumber(DispatchContext dctx, Map<String, Object> context)
+//            throws GenericServiceException, OfbizServiceException {
+//        try {
+//            Delegator delegator = dctx.getDelegator();
+//            LocalDispatcher dispatcher = dctx.getDispatcher();
+//            GenericValue userLogin = (GenericValue) context.get("userLogin");
+//            String partyId = (String) context.get("partyId");
+//            //使用成员ID查询所在部门(一个成员只能有个一个部门)
+//            GenericValue department = EntityQuery.use(delegator).from("PartyRelationship")
+//                    .where(UtilMisc.toMap("partyIdTo", partyId, "roleTypeIdTo", "ORD_EMPLOYEE"))
+//                    .queryFirst();
+//            if (UtilValidate.isEmpty(department)) {
+//                throw new OfbizServiceException("没有找到当前员工所在部门");
+//            }
+//            //当前成员所在部门Id
+//            String currentDepartmentId = department.getString("partyIdFrom");
+//            //从当前部们开始遍历知道根部门为止(暂时根部们先定死为Company,今后考虑怎么改为动态获取根部们)
+//            ServiceUtils.traverseUpdateParentDepartments(delegator, dispatcher, currentDepartmentId, userLogin);
+//        } catch (GenericEntityException | OfbizODataException e) {
+//            throw new GenericServiceException(e.getMessage());
+//        }
+//
+//        return ServiceUtil.returnSuccess();
+//    }
+
+
     public static Map<String, Object> updateMemberNumber(DispatchContext dctx, Map<String, Object> context)
             throws GenericServiceException, OfbizServiceException {
         try {
             Delegator delegator = dctx.getDelegator();
             LocalDispatcher dispatcher = dctx.getDispatcher();
             GenericValue userLogin = (GenericValue) context.get("userLogin");
-            String partyId = (String) context.get("partyId");
-            //使用成员ID查询所在部门(一个成员只能有个一个部门)
-            GenericValue department = EntityQuery.use(delegator).from("PartyRelationship")
-                    .where(UtilMisc.toMap("partyIdTo", partyId, "roleTypeIdTo", "ORD_EMPLOYEE"))
-                    .queryFirst();
-            if (UtilValidate.isEmpty(department)) {
-                throw new OfbizServiceException("没有找到当前员工所在部门");
+            //第一步查询所有部门Id
+            List<String> departmentIds = EntityQuery.use(delegator).from("PartyRole").where("roleTypeId","DEPARTMENT").getFieldList("partyId");
+            departmentIds.add("Company");
+
+            //获取老部门和新部门的所有父级部门。
+            
+            //第二步更新所有部门的成员数量
+            for(String departmentId : departmentIds){
+                List<GenericValue> allMembers = new ArrayList<>();
+                ServiceUtils.getDepartmentALlMembers(delegator, departmentId, allMembers);
+                dispatcher.runSync("banfftech.updatePartyGroup",UtilMisc.toMap("partyId",departmentId,"numEmployees",allMembers.size(),"userLogin",userLogin));
             }
-            //当前成员所在部门Id
-            String currentDepartmentId = department.getString("partyIdFrom");
-            //从当前部们开始遍历知道根部门为止(暂时根部们先定死为Company,今后考虑怎么改为动态获取根部们)
-            ServiceUtils.traverseUpdateParentDepartments(delegator, dispatcher, currentDepartmentId, userLogin);
+
         } catch (GenericEntityException | OfbizODataException e) {
             throw new GenericServiceException(e.getMessage());
         }
--- src/main/java/com/banfftech/util/ServiceUtils.java
+++ src/main/java/com/banfftech/util/ServiceUtils.java
@@ -1,22 +1,15 @@
 package com.banfftech.util;
 
-import com.dpbird.odata.OdataParts;
 import com.dpbird.odata.OfbizODataException;
-import com.dpbird.odata.edm.OdataOfbizEntity;
-import org.apache.ofbiz.base.util.UtilGenerics;
 import org.apache.ofbiz.base.util.UtilMisc;
 import org.apache.ofbiz.base.util.UtilValidate;
 import org.apache.ofbiz.entity.Delegator;
 import org.apache.ofbiz.entity.GenericEntityException;
-import org.apache.ofbiz.entity.GenericPK;
 import org.apache.ofbiz.entity.GenericValue;
 import org.apache.ofbiz.entity.condition.EntityCondition;
 import org.apache.ofbiz.entity.condition.EntityOperator;
 import org.apache.ofbiz.entity.util.EntityQuery;
-import org.apache.ofbiz.entity.util.EntityUtil;
 import org.apache.ofbiz.service.*;
-import org.apache.olingo.commons.api.data.ComplexValue;
-import org.apache.olingo.commons.api.data.Property;
 
 import java.math.BigDecimal;
 import java.util.*;
@@ -28,125 +21,22 @@ import java.util.*;
 public class ServiceUtils {
     public static BigDecimal staticBigDecimal = BigDecimal.ONE;
 
-    /**
-     * @param [delegator, dispatcher, currentDepartmentId, userLogin]
-     * @Author yyp
-     * @Description 主要功能:遍历所有父级部门,并更新其部门人员数量(此方法考虑,使用方法计算而不是直接加1)
-     * @Date 14:15 2023/9/19
-     **/
-    public static void traverseUpdateParentDepartments(Delegator delegator, LocalDispatcher dispatcher, String currentDepartmentId, GenericValue userLogin)
-            throws OfbizODataException, GenericServiceException, GenericEntityException {
-
+    public static Set<String> traverseParentDepartments(Delegator delegator, LocalDispatcher dispatcher, String currentDepartmentId)
+            throws GenericEntityException {
+        Set<String> allParentDepartmentIds = new HashSet<>();
         //获取该部门和父级部门的Relation
         GenericValue parentDepartment = EntityQuery.use(delegator).from("PartyRelationship")
                 .where(UtilMisc.toMap("partyIdTo", currentDepartmentId, "roleTypeIdTo", "DEPARTMENT"))
                 .queryFirst();
         //如果没有父级部门(则当前部门就是根部门,更新当前部门的人数)
-        if (UtilValidate.isEmpty(parentDepartment)) {
-            updateDepartmentMemberNumber(delegator, dispatcher, currentDepartmentId, staticBigDecimal, userLogin);
-            return;
+        if (UtilValidate.isNotEmpty(parentDepartment)) {
+            allParentDepartmentIds.add(parentDepartment.getString("partyIdFrom"));
+            Set<String> prentDepartmentIds = traverseParentDepartments(delegator, dispatcher, parentDepartment.getString("partyIdFrom"));
+            allParentDepartmentIds.addAll(prentDepartmentIds);
         }
-        //如果有父级部门,更新当前部门人数,并继续遍历更新父级部门
-        updateDepartmentMemberNumber(delegator, dispatcher, currentDepartmentId, staticBigDecimal, userLogin);
-        traverseUpdateParentDepartments(delegator, dispatcher, parentDepartment.getString("partyIdFrom"), userLogin);
+
+        return allParentDepartmentIds;
     }
-
-    /**
-     * @param [delegator, dispatcher, departmentId, userLogin]
-     * @Author yyp
-     * @Description 主要功能:根据部门ID,更新部门人数使其+1
-     * @Date 14:18 2023/9/19
-     **/
-
-    public static void updateDepartmentMemberNumber(Delegator delegator, LocalDispatcher dispatcher, String departmentId, BigDecimal staticBigDecimal, GenericValue userLogin)
-            throws GenericEntityException, GenericServiceException {
-
-        //查询当前部门
-        GenericValue Department = delegator.findOne("PartyGroup",
-                UtilMisc.toMap("partyId", departmentId), false);
-        //获取当前部门人数,并加一
-        Long numEmployees = Department.getLong("numEmployees");
-        numEmployees += 1;
-        dispatcher.runSync("banfftech.updatePartyGroup",
-                UtilMisc.toMap("partyId", departmentId, "numEmployees", numEmployees, "userLogin", userLogin));
-    }
-
-//    public static void traverseUpdateParentDepartments(Delegator delegator, LocalDispatcher dispatcher, String currentDepartmentId, GenericValue userLogin)
-//            throws OfbizODataException, GenericServiceException, GenericEntityException {
-//
-//        //获取该部门和父级部门的Relation
-//        GenericValue parentDepartment = EntityQuery.use(delegator).from("PartyRelationship")
-//                .where(UtilMisc.toMap("partyIdTo", currentDepartmentId, "roleTypeIdTo", "DEPARTMENT"))
-//                .queryFirst();
-//        //如果没有父级部门(则当前部门就是根部门,更新当前部门的人数)
-//        if (UtilValidate.isEmpty(parentDepartment)) {
-//            updateDepartmentMemberNumber(delegator, dispatcher, currentDepartmentId, staticBigDecimal,userLogin);
-//            return;
-//        }
-//        //如果有父级部门,更新当前部门人数,并继续遍历更新父级部门
-//        updateDepartmentMemberNumber(delegator, dispatcher, currentDepartmentId, staticBigDecimal,userLogin);
-//
-//
-//        traverseUpdateParentDepartments(delegator, dispatcher, parentDepartment.getString("partyIdFrom"), userLogin);
-//    }
-//
-//    /**
-//     * @param [delegator, dispatcher, departmentId, userLogin]
-//     * @Author yyp
-//     * @Description 主要功能:根据部门ID,更新部门人数使其+1
-//     * @Date 14:18 2023/9/19
-//     **/
-//
-//    public static void updateDepartmentMemberNumber(Delegator delegator, LocalDispatcher dispatcher, String departmentId, BigDecimal staticBigDecimal,GenericValue userLogin)
-//            throws GenericEntityException, GenericServiceException {
-//
-//        //查询当前部门
-//        GenericValue Department = delegator.findOne("PartyGroup",
-//                UtilMisc.toMap("partyId", departmentId), false);
-//        //获取当前部门人数,并加一
-////        BigDecimal numEmployees = Department.getBigDecimal("numEmployees");
-////        numEmployees = numEmployees.add(BigDecimal.ONE);
-////        dispatcher.runSync("banfftech.updatePartyGroup",
-////                UtilMisc.toMap("partyId", departmentId, "numEmployees", numEmployees, "userLogin", userLogin));
-//        List<GenericValue> membersId = EntityQuery.use(delegator).from("PartyRelationship")
-//                .where(UtilMisc.toMap("partyIdFrom", departmentId, "roleTypeIdTo", "ORD_EMPLOYEE"))
-//                .getFieldList("partyIdTo");
-//        EntityCondition membersIdCondition = EntityCondition.makeCondition("partyId", EntityOperator.IN, membersId);
-//        List<GenericValue> members = EntityQuery.use(delegator).from("PartyAndContact").where(membersIdCondition).queryList();
-//        staticBigDecimal = staticBigDecimal.add(BigDecimal.valueOf(members.size()));
-//        dispatcher.runSync("banfftech.updatePartyGroup",
-//                UtilMisc.toMap("partyId", departmentId, "numEmployees", members.size(), "userLogin", userLogin));
-//    }
-//
-//
-//    private void getDepartmentALlMembers(Delegator delegator, String departmentId, List<GenericValue> allMembers)
-//            throws OfbizODataException {
-//        //获取该部门的所有子部门
-//        try {
-//            List<GenericValue> subDepartments = EntityQuery.use(delegator).from("PartyRelationship")
-//                    .where(UtilMisc.toMap("partyIdFrom", departmentId, "roleTypeIdTo", "DEPARTMENT"))
-//                    .queryList();
-//
-//            //获取当前部门的所有成员列表
-//            List<GenericValue> membersId = EntityQuery.use(delegator).from("PartyRelationship")
-//                    .where(UtilMisc.toMap("partyIdFrom", departmentId, "roleTypeIdTo", "ORD_EMPLOYEE"))
-//                    .getFieldList("partyIdTo");
-//
-//            EntityCondition membersIdCondition = EntityCondition.makeCondition("partyId", EntityOperator.IN, membersId);
-//            List<GenericValue> members = EntityQuery.use(delegator).from("PartyAndContact").where(membersIdCondition).queryList();
-//            allMembers.addAll(members);
-//
-//            if (UtilValidate.isNotEmpty(subDepartments)) {
-//                for (GenericValue subDepartment : subDepartments) {
-//                    getDepartmentALlMembers(delegator, subDepartment.getString("partyIdTo"), allMembers);
-//                }
-//            }
-//            return;
-//        } catch (GenericEntityException e) {
-//            throw new OfbizODataException(e.getMessage());
-//        }
-//    }
-
 
     /**
      * @param [delegator, departmentId, allMembers]
@@ -154,7 +44,7 @@ public class ServiceUtils {
      * @Description //使用部门Id,查询并返回该部门及其子部门的所有成员
      * @Date 11:16 2023/10/12
      **/
-    public static List<GenericValue> getDepartmentALlMembers(Delegator delegator, String departmentId, List<GenericValue> allMembers)
+    public static void getDepartmentALlMembers(Delegator delegator, String departmentId, List<GenericValue> allMembers)
             throws OfbizODataException {
         try {
             //获取该部门的所有子部门
@@ -178,7 +68,6 @@ public class ServiceUtils {
                     getDepartmentALlMembers(delegator, subDepartment.getString("partyIdTo"), allMembers);
                 }
             }
-            return allMembers;
         } catch (GenericEntityException e) {
             throw new OfbizODataException(e.getMessage());
         }
